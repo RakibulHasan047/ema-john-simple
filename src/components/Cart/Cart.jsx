cd4b66e2539de1fc9e1d979dabb81ea08d130b5f
MdRakibulHasan <rakibhasan047534@gmail.com>
2023-03-29 21:36:36 +0600

    save cart to local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,6 +4,8 @@ import './Cart.css'
 const Cart = (props) => {
     const {cart} = props;
 
+    console.log(cart);
+
     let totalPrice = 0;
     let totalShipping = 0;
     for (const product of cart){
